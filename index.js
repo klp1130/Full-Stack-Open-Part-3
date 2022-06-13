7dc325919c391c8add4db1549d932b55c2702645
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-06-13 11:58:53 -0600

    commit on Macair

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const { request } = require('express')
 const { response } = require('express')
 const express = require('express')
 const app = express()
-
+const morgan = require('morgan')
 
 
 let persons = [
@@ -27,7 +28,20 @@ let persons = [
     }
 ]
 
+/// Middleware request logger
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    JSON.stringify(req.body)
+  ].join(' ')
+}))
+
   app.use(express.json())
+
 
 
   /// GET: an event handler that is used to handle GET made a /root:
@@ -114,7 +128,15 @@ let persons = [
     response.status(204).end()
   })
   
+  const unknownEndpoint = (request, response) => {
+    response.status(400).send({error: 'unknown endpoint'})
+  }
+
+  app.use(unknownEndpoint)
+
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
+
+  
