dccaa7fd3300d71870b0d532cbb9eb0a0a0c2c91
Keenan Lee-Peters <keenan.leepeters@gmail.com>
2022-06-24 06:45:07 -0600

    uibuild

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ app.use(morgan((tokens, req, res) => {
 
   app.use(errorHandler)
 
-  const PORT = process.env.PORT
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
