da116b9f658b488560b10ed04a43be8f1db5abc5
Keenan Lee-Peters <96672628+klp1130@users.noreply.github.com>
2022-07-24 07:55:24 -0600

    uibuild

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id)
     .then(person => {
       if (person) {
-        response.json(person.toJSON())
+        response.json(person)
       } else {
         response.status(404).end()
       }
